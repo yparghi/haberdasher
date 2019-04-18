ab0304b49ec9a25949e16639e7342a87a50f6c78
Yash Parghi <yash@daybydaycoding.com>
2019-04-17 21:07:31 -0400

    Getting the server to run in IDEA with Java 8 and Spark

--- server/src/main/java/com/haberdashervcs/server/App.java
+++ server/src/main/java/com/haberdashervcs/server/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Haberdasher!" );
+        spark.Spark.get("/hello", (request, response) -> "Hello World!");
     }
 }
