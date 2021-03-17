ab930964aa7b3e5df27db303eefc657ebd394fc3
Yash Parghi <yash@nytch.me>
2021-03-17 17:30:35 -0400

    Toss Spark and get it to run in intellij

--- server/src/main/java/com/haberdashervcs/server/App.java
+++ server/src/main/java/com/haberdashervcs/server/App.java
@@ -9,6 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Haberdasher!" );
-        spark.Spark.get("/hello", (request, response) -> "Hello World!");
     }
 }
