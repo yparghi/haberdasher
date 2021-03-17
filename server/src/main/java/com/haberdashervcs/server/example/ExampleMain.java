9e52acb0c8dc3565ec5cc76e56e8f6aedabd48e8
Yash Parghi <yash@nytch.me>
2021-03-17 17:40:29 -0400

    A little cleanup of structure

---
+++ server/src/main/java/com/haberdashervcs/server/example/ExampleMain.java
@@ -0,0 +1,12 @@
+package com.haberdashervcs.server.example;
+
+/**
+ * TODO
+ */
+public class ExampleMain {
+
+    public static void main(String[] args) {
+        System.out.println( "Hello Haberdasher!" );
+    }
+
+}
---
+++ server/src/test/java/com/haberdashervcs/server/example/ExampleMainTest.java
@@ -0,0 +1,16 @@
+package com.haberdashervcs.server.example;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class ExampleMainTest  {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+
+}
