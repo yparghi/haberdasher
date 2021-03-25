0af88c6ceb0e5ce96d7c0ad2c54d95f5a91a165d
Yash Parghi <yash@daybydaycoding.com>
2021-03-24 20:09:42 -0400

    Stub in the first builder/configurator

---
+++ server/src/main/java/com/haberdashervcs/config/HaberdasherServer.java
@@ -0,0 +1,37 @@
+package com.haberdashervcs.config;
+
+import com.haberdashervcs.data.HdDatastore;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+
+public final class HaberdasherServer {
+
+    public static final class Builder {
+        private HdDatastore datastore = null;
+
+        private Builder() {}
+
+        public Builder withDatastore(HdDatastore datastore) {
+            checkState(datastore == null);
+            this.datastore = checkNotNull(datastore);
+            return this;
+        }
+
+        public HaberdasherServer build() {
+            return new HaberdasherServer(datastore);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+    private final HdDatastore datastore;
+
+    private HaberdasherServer(HdDatastore datastore) {
+        this.datastore = checkNotNull(datastore);
+    }
+}
--- server/src/main/java/com/haberdashervcs/server/example/ExampleMain.java
+++ server/src/main/java/com/haberdashervcs/server/example/ExampleMain.java
@@ -1,5 +1,8 @@
 package com.haberdashervcs.server.example;
 
+import com.haberdashervcs.config.HaberdasherServer;
+import com.haberdashervcs.data.hbase.HBaseDatastore;
+
 /**
  * TODO
  */
@@ -7,6 +10,12 @@ public class ExampleMain {
 
     public static void main(String[] args) {
         System.out.println( "Hello Haberdasher!" );
+
+        HaberdasherServer server = HaberdasherServer.builder()
+                .withDatastore(new HBaseDatastore())
+                .build();
+
+        System.out.println("Done!");
     }
 
 }
