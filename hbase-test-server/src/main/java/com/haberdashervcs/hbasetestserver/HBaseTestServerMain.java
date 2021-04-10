ac7643ddf6efc040b77293964f844c4c45d02a9e
Yash Parghi <yash@daybydaycoding.com>
2021-04-10 13:00:08 -0400

    Test works with a CLI cluster, but not my java test cluster...

--- hbase-test-server/src/main/java/com/haberdashervcs/hbasetestserver/HBaseTestServerMain.java
+++ hbase-test-server/src/main/java/com/haberdashervcs/hbasetestserver/HBaseTestServerMain.java
@@ -1,5 +1,10 @@
 package com.haberdashervcs.hbasetestserver;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
 import org.apache.log4j.Logger;
 
 
@@ -13,7 +18,11 @@ public class HBaseTestServerMain {
     public static void main(String[] args) throws Exception {
         LOG.info("Starting the HBase test server...");
 
-        HBaseTestClusterManager testCluster = HBaseTestClusterManager.getInstance();
-        testCluster.setUp();
+        Configuration config = HBaseConfiguration.create();
+        HBaseTestingUtility testUtil = new HBaseTestingUtility(config);
+        testUtil.startMiniCluster();
+
+        System.out.println("Master: " + testUtil.getConfiguration().get("hbase.master", "<none>"));
+        System.out.println("Done.");
     }
 }
--- server/src/test/java/com/haberdashervcs/server/datastore/hbase/HBaseDatastoreTest.java
+++ server/src/test/java/com/haberdashervcs/server/datastore/hbase/HBaseDatastoreTest.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.protobuf.ByteString;
+import com.haberdashervcs.common.logging.HdLogger;
+import com.haberdashervcs.common.logging.HdLoggers;
 import com.haberdashervcs.common.objects.CommitEntry;
 import com.haberdashervcs.common.objects.FolderListing;
 import com.haberdashervcs.common.protobuf.CommitsProto;
@@ -20,10 +22,17 @@ import com.haberdashervcs.server.operations.CheckoutStream;
 import com.haberdashervcs.server.operations.change.AddChange;
 import com.haberdashervcs.server.operations.change.ApplyChangesetResult;
 import com.haberdashervcs.server.operations.change.Changeset;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ColumnFamilyDescriptorBuilder;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.TableDescriptor;
+import org.apache.hadoop.hbase.client.TableDescriptorBuilder;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.After;
 import org.junit.Before;
@@ -33,22 +42,59 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
+// Assumes an HBase test cluster is already running -- see HBaseTestServerMain in the hbase-test-server module for a
+// simple Java app you can start/stop.
+//
+// NOTE: I'd like to set up the test cluster right here in these JUnit tests, but the HBase test lib has all kinds of
+// dependencies I don't want on my classpath. I think for convenience, HBase has to be running separately.
 public class HBaseDatastoreTest {
 
-    private HBaseTestClusterManager clusterManager;
+    private static final HdLogger LOG = HdLoggers.create(HBaseDatastoreTest.class);
+
     private Connection conn;
 
 
     @Before
     public void setUp() throws Exception {
-        clusterManager = HBaseTestClusterManager.getInstance();
-        clusterManager.setUp();
-        conn = clusterManager.getConn();
+        Configuration conf = HBaseConfiguration.create();
+        conf.clear();
+        conf.set("hbase.master", "localhost:60000");
+        conn = ConnectionFactory.createConnection(conf);
+
+        createTables();
+    }
+
+    private void createTables() throws Exception {
+        LOG.info("Creating test tables.");
+
+        Admin admin = conn.getAdmin();
+
+        TableDescriptor filesTableDesc = TableDescriptorBuilder
+                .newBuilder(TableName.valueOf("Files"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("cfMain"))
+                .build();
+        admin.createTable(filesTableDesc);
+
+        TableDescriptor foldersTableDesc = TableDescriptorBuilder
+                .newBuilder(TableName.valueOf("Folders"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("cfMain"))
+                .build();
+        admin.createTable(foldersTableDesc);
+
+        TableDescriptor commitsTableDesc = TableDescriptorBuilder
+                .newBuilder(TableName.valueOf("Commits"))
+                .setColumnFamily(ColumnFamilyDescriptorBuilder.of("cfMain"))
+                .build();
+        admin.createTable(commitsTableDesc);
     }
 
     @After
     public void tearDown() throws Exception {
-        clusterManager.tearDownBetweenTests();
+        /*Admin admin = conn.getAdmin();
+        for (String tableName : Arrays.asList("Files", "Folders", "Commits")) {
+            admin.disableTable(TableName.valueOf(tableName));
+            admin.deleteTable(TableName.valueOf(tableName));
+        }*/
     }
 
 
