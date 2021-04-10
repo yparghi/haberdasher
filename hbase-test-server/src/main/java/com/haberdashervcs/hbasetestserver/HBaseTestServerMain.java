70d3bc07ab46e21d8d2aedbef51b1f2539fc59b2
Yash Parghi <yash@daybydaycoding.com>
2021-04-10 15:55:06 -0400

    More manual fixing: Try to work around the WAL error

--- hbase-test-server/src/main/java/com/haberdashervcs/hbasetestserver/HBaseTestServerMain.java
+++ hbase-test-server/src/main/java/com/haberdashervcs/hbasetestserver/HBaseTestServerMain.java
@@ -38,6 +38,12 @@ public class HBaseTestServerMain {
         Configuration conf = HBaseConfiguration.create();
         conf.clear();
         conf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181");
+        conf.set("hbase.wal.dir", "/tmp/yashhbase/wal");
+        conf.set("hbase.rootdir", "/tmp/yashhbase/hbase");
+        conf.set("hbase.tmp.dir", "/tmp/yashhbase/hbasetmp");
+        conf.set("hbase.cluster.distributed", "false");
+        // This prevents error messages with the WAL -- by disabling it, I think.
+        conf.set("hbase.unsafe.stream.capability.enforce", "false");
 
         HBaseTestingUtility testUtil = new HBaseTestingUtility();  // TEMP
 
@@ -46,6 +52,9 @@ public class HBaseTestServerMain {
         Path zkPath = Files.createTempDirectory("yashzk");
         zk.startup(zkPath.toFile());
 
+        // Current bug:
+        // 2021-04-10 15:41:49,942 ERROR [master/192.168.1.2:16000:becomeActiveMaster] wal.AsyncFSWALProvider (AsyncFSWALProvider.java:createAsyncWriter(117)) - The RegionServer async write ahead log provider relies on the ability to call hflush for proper operation during component failures, but the current FileSystem does not support doing so. Please check the config value of 'hbase.wal.dir' and ensure it points to a FileSystem mount that has suitable capabilities for output streams.
+        // See: https://hbase.apache.org/2.0/book.html
         MiniHBaseCluster hBase = new MiniHBaseCluster(conf, 1);
     }
 }
